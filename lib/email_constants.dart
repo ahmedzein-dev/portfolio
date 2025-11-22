df21e4cc704960e53305904255c266bab515ad29
Ahmed Zein <ahmedzezo1080@gmail.com>
2025-11-22 19:52:23 +0400

    Deploy new Flutter web build

--- lib/email_constants.dart
+++ lib/email_constants.dart
@@ -1,5 +1,5 @@
 class EmailConstants {
-  static const String serviceId = 'service_8i6c89x';
-  static const String templateId = 'template_8y5elfg';
-  static const String publicKey = 'AWhK80EypqJMXG8ho';
+  static const String serviceId = '';
+  static const String templateId = '';
+  static const String publicKey = '';
 }
